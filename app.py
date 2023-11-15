b6e1ad320e4df2fd646cc63dfd3984e38f35b00e
daz <71845855+dazscripts@users.noreply.github.com>
2023-11-15 01:12:06 +0000

    https is funny

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import openai
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
